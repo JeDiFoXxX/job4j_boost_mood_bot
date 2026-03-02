c8f4abd0a0bd370f9e23aaf4914356ea4729e100
JeDiFoXxX <serjik1993991@gmail.com>
2026-03-02 17:18:34 +0300

    Реализация BeanNameAware

--- src/main/java/ru/job4j/infastructur/LifecycleComponent.java
+++ src/main/java/ru/job4j/infastructur/LifecycleComponent.java
@@ -3,14 +3,23 @@ package ru.job4j.infastructur;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 
-public abstract class LifecycleComponent {
+import org.springframework.beans.factory.BeanNameAware;
+
+public abstract class LifecycleComponent implements BeanNameAware {
+    private String beanName;
+
+    @Override
+    public void setBeanName(String name) {
+        this.beanName = name;
+    }
+
     @PostConstruct
     private void initMethod() {
-        System.out.printf("Бин %s создан%n", this.getClass().getSimpleName());
+        System.out.printf("Бин %s создан%n", beanName);
     }
 
     @PreDestroy
     private void destroyMethod() {
-        System.out.printf("Бин %s готов к удалению%n", this.getClass().getSimpleName());
+        System.out.printf("Бин %s готов к удалению%n", beanName);
     }
 }
