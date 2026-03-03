a2934a024f4acc103904a2f648fffe67b4c926b7
JeDiFoXxX <serjik1993991@gmail.com>
2026-03-03 16:07:37 +0300

    Настройка тестового репозитория | @Profile(test)

---
+++ src/main/java/ru/job4j/model/User.java
@@ -0,0 +1,3 @@
+package ru.job4j.model;
+
+public record User(Long id, long clientId, long chatId) { }
---
+++ src/main/java/ru/job4j/repository/test/UserRepository.java
@@ -0,0 +1,11 @@
+package ru.job4j.repository.test;
+
+import java.util.List;
+
+import ru.job4j.model.User;
+
+public interface UserRepository {
+    List<User> findAll();
+
+    User findByClientId(Long clientId);
+}
