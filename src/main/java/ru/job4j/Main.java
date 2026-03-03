681d35bf27846a892450f464920ddd787e5af4cd
JeDiFoXxX <serjik1993991@gmail.com>
2026-03-03 12:52:23 +0300

    Мои настройки

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,6 +2,7 @@ package ru.job4j;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
