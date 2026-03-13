ada2a2adb53037fd922a03bbdfa011d8687329d6
JeDiFoXxX <serjik1993991@gmail.com>
2026-03-13 20:07:28 +0300

    Обработка исключений с использованием AOP

--- src/main/java/ru/job4j/aop/ExceptionHandlingAspect.java
+++ src/main/java/ru/job4j/aop/ExceptionHandlingAspect.java
@@ -1,4 +1,19 @@
 package ru.job4j.aop;
 
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Aspect
+@Component
 public class ExceptionHandlingAspect {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandlingAspect.class);
+
+    @AfterThrowing(pointcut = "execution(* ru.job4j..*.*(..))", throwing = "ex")
+    public void handleException(Exception ex) {
+        LOGGER.error("An error occurred: {}", ex.getMessage());
+    }
 }
