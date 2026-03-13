4717c7a51e67afbe0b849340ff1e4e64b384dfe0
JeDiFoXxX <serjik1993991@gmail.com>
2026-03-13 20:21:59 +0300

    Концепция AOP

--- src/main/java/ru/job4j/aop/LoggingAspect.java
+++ src/main/java/ru/job4j/aop/LoggingAspect.java
@@ -1,4 +1,20 @@
 package ru.job4j.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import org.aspectj.lang.JoinPoint;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
 public class LoggingAspect {
+    @Before("execute(ru.job4j.service..*.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.printf("Вызов метода: %s c аргументами %s%n",
+                joinPoint.getSignature().getName(),
+                Arrays.toString(joinPoint.getArgs()));
+    }
 }
